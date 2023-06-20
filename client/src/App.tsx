0854f0fb919c413a49e1994e33823abf15c7c9a7
andrewbidylo <bidyloandrew@gmail.com>
2023-06-20 15:23:25 -0700

    add routing and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,30 @@
-import {CssBaseline, ThemeProvider} from "@mui/material"
-import {createTheme} from "@mui/material/styles"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
 import { themeSettings } from "./theme"
 import { useMemo } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "@/pages/navbar"
 
 function App() {
-  const theme = useMemo(()=> createTheme(themeSettings),[])
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <>
       <div className="app">
-        <ThemeProvider theme={theme}>
-          <CssBaseline>
-            
-          </CssBaseline>
-        </ThemeProvider>
+        <BrowserRouter>
+
+          <ThemeProvider theme={theme}>
+            <CssBaseline>
+              <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar/>
+              <Routes>
+                <Route path="/" element={<>Dashboard page</>}/>
+                <Route path="/prediction" element={<>Prediction page</>}/>
+                <Route/>
+              </Routes>
+              </Box>
+            </CssBaseline>
+          </ThemeProvider>
+        </BrowserRouter>
       </div>
     </>
   )
